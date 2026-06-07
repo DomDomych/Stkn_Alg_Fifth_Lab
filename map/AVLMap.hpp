3a232fd63e26f33ca6984d92afc24fb0ec99d030
Damir <damirmatematic@gmail.com>
2026-06-07 21:06:27 +0300

    Added templates, not fully done, but now AVLMap<string,string> is specialized

--- map/AVLMap.hpp
+++ map/AVLMap.hpp
@@ -5,9 +5,9 @@
 #include <vector>
 
 template <typename Key, typename Value>
-class AVLTree
+class AVLTreeBase
 {
-private:
+protected:
     struct Node
     {
         Key key;
@@ -17,26 +17,24 @@ private:
         unsigned int h;
         int count;
         Node(const Key &key, const Value &value) : key(key),
-                                                                 value(value),
-                                                                 left(nullptr),
-                                                                 right(nullptr),
-                                                                 h(1),
-                                                                 count(0) {};
+                                                   value(value),
+                                                   left(nullptr),
+                                                   right(nullptr),
+                                                   h(1),
+                                                   count(0)
+                                                   {}
         ~Node() = default;
     };
     Node *root;
 
-    unsigned int height(Node *p)
+        unsigned int height(Node *p)
     {
         return p ? p->h : 0;
     }
 
     int bfactor(Node *p)
     {
-        unsigned int hl = p->left ? height(p->left) : 0;
-        unsigned int hr = p->right ? height(p->right) : 0;
-
-        return static_cast<int>(hr) - static_cast<int>(hl);
+        return static_cast<int>(height(p->right)) - static_cast<int>(height(p->left));
     }
 
     void fixheight(Node *p)
@@ -47,7 +45,7 @@ private:
         p->h = (hl > hr ? hl : hr) + 1;
     }
 
-    Node *RotateRight(Node *p)
+    Node *rotateRight(Node *p)
     {
         Node *q = p->left;
         p->left = q->right;
@@ -57,7 +55,7 @@ private:
         return q;
     }
 
-    Node *RotateLeft(Node *p)
+    Node *rotateLeft(Node *p)
     {
         Node *q = p->right;
         p->right = q->left;
@@ -74,17 +72,17 @@ private:
         {
             if (bfactor(p->right) < 0)
             {
-                p->right = RotateRight(p->right);
+                p->right = rotateRight(p->right);
             }
-            return RotateLeft(p);
+            return rotateLeft(p);
         }
         if (bfactor(p) == -2)
         {
             if (bfactor(p->left) > 0)
             {
-                p->left = RotateLeft(p->left);
+                p->left = rotateLeft(p->left);
             }
-            return RotateRight(p);
+            return rotateRight(p);
         }
         return p;
     }
@@ -109,7 +107,7 @@ private:
         return balance(p);
     }
 
-    Node *find(Node *p, const Key &key) const
+        Node *find(Node *p, const Key &key) const
     {
         if (!p)
         {
@@ -125,8 +123,7 @@ private:
         }
         return p;
     }
-
-    Node *MostPopular(Node *p, Node *best) const
+    Node *mostPopular(Node *p, Node *best) const
     {
         if (!p)
             return best;
@@ -136,8 +133,8 @@ private:
             best = p;
         }
 
-        best = MostPopular(p->left, best);
-        best = MostPopular(p->right, best);
+        best = mostPopular(p->left, best);
+        best = mostPopular(p->right, best);
 
         return best;
     }
@@ -207,19 +204,26 @@ private:
         print(p->right, number);
     }
 
+};
+
+template <typename Key, typename Value>
+class AVLTree:public AVLTreeBase<Key,Value>
+{
+using Node = typename AVLTreeBase<Key,Value>::Node;
+
 public:
     bool remove(const Key &key)
     {
-        if (!find(root, key))
+        if (!this->find(this->root, key))
             return false;
 
-        root = remove(root, key);
+        this->root = AVLTreeBase<Key,Value>::remove(this->root, key);
         return true;
     }
 
     bool get(const Key &key, Value &value) const
     {
-        Node *found = find(root, key);
+        Node *found = this->find(this->root, key);
 
         if (!found)
         {
@@ -233,7 +237,7 @@ public:
 
     bool getMostPopular(Key &key, Value &value, int &count) const
     {
-        Node *best = MostPopular(root, nullptr);
+        Node *best = this->mostPopular(this->root, nullptr);
 
         if (!best)
             return false;
@@ -247,12 +251,12 @@ public:
 
     Value &operator[](const Key &key)
     {
-        Node *found = find(root, key);
+        Node *found = this->find(this->root, key);
 
         if (!found)
         {
-            root = insert(root, key, Value{});
-            found = find(root, key);
+            this->root = this->insert(this->root, key, Value{});
+            found = this->find(this->root, key);
         }
 
         return found->value;
@@ -261,18 +265,18 @@ public:
     void print() const
     {
         int number = 1;
-        print(root, number);
+        this->print(this->root, number);
     }
 
     void increment(const Key &key)
     {
-        Node *found = find(root, key);
+        Node *found = this->find(this->root, key);
 
         if (found)
         {
             found->count++;
         }
-        return;
+
     }
 
     AVLTree(const AVLTree &) = delete;
@@ -280,68 +284,30 @@ public:
 
     AVLTree()
     {
-        root = nullptr;
+        this->root = nullptr;
     }
 
     ~AVLTree()
     {
-        clear(root);
-        root = nullptr;
+        this->clear(this->root);
+        this->root = nullptr;
     }
 };
 
-
-
-
-
-//Specialization for std::string
-template<>
-class AVLTree<std::string,std::string>
+// Specialization for std::string
+template <>
+class AVLTree<std::string, std::string>: public AVLTreeBase<std::string,std::string>
 {
+
+using Node = typename AVLTreeBase<std::string,std::string>::Node; 
+
+
 private:
-    struct Node
-    {
-        std::string key;
-        std::string value;
-        Node *left;
-        Node *right;
-        unsigned int h;
-        int count;
-        Node(const std::string &key, const std::string &value) : key(key),
-                                                                 value(value),
-                                                                 left(nullptr),
-                                                                 right(nullptr),
-                                                                 h(1),
-                                                                 count(0) {};
-        ~Node() = default;
-    };
-    Node *root;
-
-    unsigned int height(Node *p)
-    {
-        return p ? p->h : 0;
-    }
-
-    int bfactor(Node *p)
-    {
-        unsigned int hl = p->left ? height(p->left) : 0;
-        unsigned int hr = p->right ? height(p->right) : 0;
-
-        return static_cast<int>(hr) - static_cast<int>(hl);
-    }
-
-    void fixheight(Node *p)
-    {
-        unsigned int hl = height(p->left);
-        unsigned int hr = height(p->right);
-
-        p->h = (hl > hr ? hl : hr) + 1;
-    }
 
     std::vector<int> prefixfunction(const std::string &s) const
     {
         std::vector<int> pi(s.size(), 0);
-        for (int i = 1; i < s.size(); i++)
+        for (size_t i = 1; i < s.size(); i++)
         {
             int j = pi[i - 1];
             while (j > 0 && s[i] != s[j])
@@ -358,16 +324,16 @@ private:
         return pi;
     }
 
-    bool kmp(const std::string &value, const std::string  &word) const
+    bool kmp(const std::string &value, const std::string &word) const
     {
         if (word.empty())
-            return false;
+            return true;
 
         std::vector<int> pi = prefixfunction(word);
 
         int j = 0;
 
-        for (int i = 0; i < value.size(); i++)
+        for (size_t i = 0; i < value.size(); i++)
         {
             while (j > 0 && value[i] != word[j])
             {
@@ -386,168 +352,7 @@ private:
         }
         return false;
     }
-
-    Node *RotateRight(Node *p)
-    {
-        Node *q = p->left;
-        p->left = q->right;
-        q->right = p;
-        fixheight(p);
-        fixheight(q);
-        return q;
-    }
-
-    Node *RotateLeft(Node *p)
-    {
-        Node *q = p->right;
-        p->right = q->left;
-        q->left = p;
-        fixheight(p);
-        fixheight(q);
-        return q;
-    }
-
-    Node *balance(Node *p)
-    {
-        fixheight(p);
-        if (bfactor(p) == 2)
-        {
-            if (bfactor(p->right) < 0)
-            {
-                p->right = RotateRight(p->right);
-            }
-            return RotateLeft(p);
-        }
-        if (bfactor(p) == -2)
-        {
-            if (bfactor(p->left) > 0)
-            {
-                p->left = RotateLeft(p->left);
-            }
-            return RotateRight(p);
-        }
-        return p;
-    }
-
-    Node *insert(Node *p, const std::string &key, const std::string &value)
-    {
-        if (!p)
-            return new Node(key, value);
-        if (key < p->key)
-        {
-            p->left = insert(p->left, key, value);
-        }
-        else if (key == p->key)
-        {
-            p->value = value;
-            return p;
-        }
-        else
-        {
-            p->right = insert(p->right, key, value);
-        }
-        return balance(p);
-    }
-
-    Node *find(Node *p, const std::string &key) const
-    {
-        if (!p)
-        {
-            return nullptr;
-        }
-        if (key < p->key)
-        {
-            return find(p->left, key);
-        }
-        if (key > p->key)
-        {
-            return find(p->right, key);
-        }
-        return p;
-    }
-
-    Node *MostPopular(Node *p, Node *best) const
-    {
-        if (!p)
-            return best;
-
-        if (!best || p->count > best->count)
-        {
-            best = p;
-        }
-
-        best = MostPopular(p->left, best);
-        best = MostPopular(p->right, best);
-
-        return best;
-    }
-
-    Node *findmin(Node *p)
-    {
-        return p->left ? findmin(p->left) : p;
-    }
-
-    Node *removemin(Node *p)
-    {
-        if (p->left == nullptr)
-        {
-            return p->right;
-        }
-
-        p->left = removemin(p->left);
-        return balance(p);
-    }
-
-    Node *remove(Node *p, const std::string &key)
-    {
-        if (!p)
-            return nullptr;
-
-        if (key < p->key)
-        {
-            p->left = remove(p->left, key);
-        }
-        else if (key > p->key)
-        {
-            p->right = remove(p->right, key);
-        }
-        else
-        {
-            Node *l = p->left;
-            Node *r = p->right;
-            delete p;
-            if (!r)
-                return l;
-            Node *min = findmin(r);
-            min->right = removemin(r);
-            min->left = l;
-            return balance(min);
-        }
-        return balance(p);
-    }
-
-    void clear(Node *p)
-    {
-        if (!p)
-            return;
-
-        clear(p->left);
-        clear(p->right);
-
-        delete p;
-    }
-
-    void print(Node *p, int &number) const
-    {
-        if (!p)
-            return;
-        print(p->left, number);
-        std::cout << number << ". " << p->key << "->" << p->value << ' ' << p->count << '\n';
-        number++;
-        print(p->right, number);
-    }
-
-
+    
     void find_by_word(Node *p, const std::string &word, std::vector<std::pair<std::string, std::string>> &found) const
     {
         if (!p)
@@ -558,37 +363,13 @@ private:
             found.push_back({p->key, p->value});
         }
         find_by_word(p->right, word, found);
-        return;
     }
-
 
 public:
-    bool remove(const std::string &key)
-    {
-        if (!find(root, key))
-            return false;
-
-        root = remove(root, key);
-        return true;
-    }
-
-    bool get(const std::string &key, std::string &value) const
-    {
-        Node *found = find(root, key);
-
-        if (!found)
-        {
-            return false;
-        }
-
-        value = found->value;
-
-        return true;
-    }
 
     bool getMostPopular(std::string &key, std::string &value, int &count) const
     {
-        Node *best = MostPopular(root, nullptr);
+        Node *best = this->mostPopular(this->root, nullptr);
 
         if (!best)
             return false;
@@ -600,41 +381,11 @@ public:
         return true;
     }
 
-    std::string &operator[](const std::string &key)
-    {
-        Node *found = find(root, key);
-
-        if (!found)
-        {
-            root = insert(root, key, "");
-            found = find(root, key);
-        }
-
-        return found->value;
-    }
-
     std::vector<std::pair<std::string, std::string>> find_by_word(const std::string &word) const
     {
         std::vector<std::pair<std::string, std::string>> found;
-        find_by_word(root, word, found);
+        find_by_word(this->root, word, found);
         return found;
-    }
-
-    void print() const
-    {
-        int number = 1;
-        print(root, number);
-    }
-
-    void increment(const std::string &key)
-    {
-        Node *found = find(root, key);
-
-        if (found)
-        {
-            found->count++;
-        }
-        return;
     }
 
     AVLTree(const AVLTree &) = delete;
@@ -642,12 +393,12 @@ public:
 
     AVLTree()
     {
-        root = nullptr;
+        this->root = nullptr;
     }
 
     ~AVLTree()
     {
-        clear(root);
-        root = nullptr;
+        this->clear(this->root);
+        this->root = nullptr;
     }
 };
